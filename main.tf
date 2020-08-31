540fbd84a2905d06c54bb23d2889f4bffea22809
Ross Clarke <ross@lnpr.info>
2020-08-31 08:25:35 -0400

    Change Server Size

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   name                = "example-machine"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  size                = "Standard_F2"
+  size                = "Standard_A7"
   admin_username      = "adminuser"
   admin_password      = var.admin_password
 
