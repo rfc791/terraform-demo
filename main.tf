f4018b6287c025029b0cbde5cc024b4d69e75ac1
Ross Clarke <ross@lnpr.info>
2020-08-31 08:07:45 -0400

    Create Server

--- main.tf
+++ main.tf
@@ -1,11 +1,43 @@
-resource "azurerm_resource_group" "demo" {
+resource "azurerm_resource_group" "example" {
   name     = "demo-rg"
   location = "Canada Central"
 }
 
-module "websrv01" {
-  source              = "Azure/compute/azurerm"
-  resource_group_name = azurerm_resource_group.demo.name
-  vm_os_simple        = "UbuntuServer"
-  vnet_subnet_id      = var.server_subnet
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = var.admin_password
+
+  disable_password_authentication = true
+  
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
 }
